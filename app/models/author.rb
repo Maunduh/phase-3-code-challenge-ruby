c1aa14b6fb6ddbe6b5ba4826e0fe63321932df0f
amosmaundundu <amosmaundu5@gmail.com>
2022-11-14 12:50:51 +0300

    done

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,37 @@
 class Author
-  attr_accessor :name,  :email
+  attr_reader :name
 
-
-  def initialize(name, email)
+  def initialize(name)
     @name = name
-    @email = email
-  
+
+    @articles = []
+    @magazines = []
+    if name.is_a?(String)
+      @name = name
+    else
+      puts "Expected name.class to be a String"
+    end
+  end
+
+  def articles 
+    @articles
+  end
+
+  def magazines
+    @magazines.uniq
+  end
+
+  def add_article(magazine, title)
+    if(magazine.is_a?(Magazine) && title.is_a?(String))
+      @articles  << Article.new(self, magazine, title)
+      @magazines << magazine
+    else
+      puts "Expected Magazine.class to be an Object, title.class to be a String"
+    end
+  end
+
+  def topic_areas
+    @magazines.map {|magazine| magazine.category}.uniq
   end
 
 
--- tools/console.rb
+++ tools/console.rb
@@ -7,7 +7,21 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+# Author class initialization test
+amos = Author.new("Amos")
+ken = Author.new("Ken")
+lorena = Author.new("Lorena")
 
+# Magazine class initialization test
+match_of_the_day = Magazine.new("Match of the day", "Football")
+people_magazine = Magazine.new("People magazine", "Entertainment")
+homes_kenya_magazine = Magazine.new("Homes Kenya magazine", "Real Estate")
+
+
+# Article class initialization test
+amos.add_article(football, "6 facts you don't know about Thiery Henry")
+ken.add_article(real estate, "Kilimani home search guide")
+lorena.add_article(technology, "Why you need to keep up with tech")
 
 
 
